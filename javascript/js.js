2c64048015dacf9ff0af5a443f9fe37278ea042c
Patima Imanalieva <96161014+Imnlv@users.noreply.github.com>
2023-06-16 15:39:10 +0300

    Update js.js

--- javascript/js.js
+++ javascript/js.js
@@ -42,3 +42,25 @@ if (animItems.length > 0){
     }, 300);
 }
 
+
+const rippleButton = document.querySelector(".ripple-button");
+
+function mousePositionToCustomProp(event, element) {
+  let posX = event.offsetX;
+  let posY = event.offsetY;
+
+  element.style.setProperty("--x", posX + "px");
+  element.style.setProperty("--y", posY + "px");
+}
+
+rippleButton.addEventListener("click", (e) => {
+  mousePositionToCustomProp(e, rippleButton);
+  rippleButton.classList.add("pulse");
+  rippleButton.addEventListener(
+    "animationend",
+    () => {
+      rippleButton.classList.remove("pulse");
+    },
+    { once: true }
+  );
+});
